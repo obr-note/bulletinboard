79dd7dd49f6b35ce99ee22473613703cffea23f6
obr-note <k33mdukb9n@yahoo.co.jp>
2020-03-19 11:07:35 +0900

    #4 ユーザー登録機能#2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,47 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
   
+    if @user.save
+      redirect_to user_url(@user), notice: "ユーザー「#{@user.email}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to user_url(@user), notice: "ユーザー「#{@user.email}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to new_user, notice: "ユーザー「#{@user.email}」を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
